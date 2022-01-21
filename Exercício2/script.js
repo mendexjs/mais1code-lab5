70538a27174d530211f977de80be76f06d4c91ee
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2022-01-21 20:07:38 -0300

    Criação do switch onde é escolhida qual operação matemática a ser utilizada.

---
+++ Exercício2/script.js
@@ -0,0 +1,29 @@
+let number1 = 200;
+let number2 =  25;
+let multiplicacao = number1 * number2;   
+let soma = number1 + number2;
+let subtracao = number1 - number2;
+let divisao = number1 / number2;
+let operacao = divisao;
+
+switch (operacao) {
+    case soma:
+        // console.log('O valor da soma é: ' +soma)
+        break;
+
+    case subtracao:
+        //console.log('O valor sa subtração é: ' +subtracao)
+        break;
+
+    case divisao:
+        console.log('O valor da divisao é: ' +divisao)
+    break;
+
+    case multiplicacao:
+    //  console.log('O valor da operação e: ' +multiplicacao); 
+        break;
+
+    default:
+        break;
+
+}
